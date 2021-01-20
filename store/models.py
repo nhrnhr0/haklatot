e298c42c4afbebd10a57fcd08c4940ea9138751f
nhrnhr0 <ronionsegal@gmail.com>
2021-01-20 17:17:41 +0200

    working on checkout

--- store/models.py
+++ store/models.py
@@ -4,5 +4,8 @@ from django.db import models
 class Product(models.Model):
     title = models.CharField(max_length=200)
     description = models.CharField(max_length=500)
-    image1 = models.ImageField(upload_to="upload")
-    image2 = models.ImageField(upload_to="upload", null=True)
+    image1 = models.ImageField(upload_to="upload", verbose_name="squere image")
+    image2 = models.ImageField(upload_to="upload", null=True, verbose_name="circle image")
+
+    def __str__(self):
+        return self.title
